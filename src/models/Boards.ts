db2213ef71db765229d3fdfac29166a5d1b51f37
Artyom Sazonov <sazonau.artiom@yandex.by>
2022-05-31 11:31:32 +0300

    add canMove for ever figure

--- src/models/Boards.ts
+++ src/models/Boards.ts
@@ -6,6 +6,7 @@ import { Queen } from './figures/Queen';
 import { Bishop } from './figures/Bishop';
 import { Rook } from './figures/Rook';
 import { Knight } from './figures/Knight';
+
 export class Board {
     cells: Cell[][] = []
 
@@ -20,6 +21,23 @@ export class Board {
                 }
             }
             this.cells.push(row);
+        }
+    }
+
+    public getCopyBoard(): Board {
+        const newBoard = new Board();
+        newBoard.cells = this.cells;
+        return newBoard;
+    }
+
+    public highlightCells(selectedCell: Cell | null) {
+        for (let i = 0; i < this.cells.length; i++) {
+            const row = this.cells[i];
+            for (let j = 0; j < row.length; j++) {
+                const target = row[j];
+                target.available = !!selectedCell?.figure?.canMove(target);
+            }
+
         }
     }
 
--- src/models/figures/Bishop.ts
+++ src/models/figures/Bishop.ts
@@ -5,9 +5,16 @@ import blackLogo from "../../assets/black-bishop.png";
 import whiteLogo from "../../assets/white-bishop.png";
 
 export class Bishop extends Figure {
-constructor(color: Colors, cell: Cell) {
+    constructor(color: Colors, cell: Cell) {
         super(color, cell);
-        this.logo = color === Colors.BLACK ? blackLogo: whiteLogo
+        this.logo = color === Colors.BLACK ? blackLogo : whiteLogo
         this.name = FigureNames.BISHOP
+    }
+
+    canMove(target: Cell): boolean {
+        if (!super.canMove(target)) {
+            return false;
+        }
+        return true
     }
 }
